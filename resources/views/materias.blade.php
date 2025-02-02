480d452f3252b4ff48912d1707e27299e16cada8
ferchas293855 <ferchas2015@gmail.com>
2025-02-02 17:33:40 -0600

    agregue un mejor diseño asi como un background para la seccion de materias

--- resources/views/materias.blade.php
+++ resources/views/materias.blade.php
@@ -1,32 +1,33 @@
 <x-esqueleto>
     <x-nav-bar></x-nav-bar>
-    <section class="bg-white">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-            <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-blue-900 dark:text-gray">Materias</h2>
+    <section class="bg-white pt-6">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 
+    border-4 border-gray-300 rounded-lg bg-gray-200 mt-0 pt-0   ">
+            <div class="mx-auto max-w-screen-sm text-center mb-0 lg:mb-0">
+                <h2 class="text-4xl tracking-tight font-extrabold text-blue-900 dark:text-gray mb-3 pb-3">Materias</h2>
             </div>
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm 
-                    hover:border-4 hover:border-blue-900 hover:bg-gray-100 dark:bg-blue-800 dark:border-blue-900 
-                    dark:hover:bg-blue-700 transition-all duration-3000 ease-in-out">
+                <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md
+                            dark:bg-blue-800 dark:border-blue-900 
+                            dark:hover:bg-blue-600 transition-all duration-[1000ms] ease-in-out
+                            hover:shadow-xl hover:border-blue-900">
                     <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Matemáticas</h6>
-                    <p class="font-normal text-white dark:text-white">Descripción de cada materia</p>
+                    <p class="font-normal text-white">Descripción de cada materia</p>
                 </a>
-                <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm 
-                    hover:border-4 hover:border-blue-900 hover:bg-gray-100 dark:bg-blue-800 dark:border-blue-900 
-                    dark:hover:bg-blue-700 transition-all duration-3000 ease-in-out">
-                    <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Fisica</h6>
-                    <p class="font-normal text-white dark:text-white">Descripción de cada materia</p>
+                <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md
+                            dark:bg-blue-800 dark:border-blue-900 
+                            dark:hover:bg-blue-600 transition-all duration-[1000ms] ease-in-out
+                            hover:shadow-xl hover:border-blue-900">
+                    <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Matemáticas</h6>
+                    <p class="font-normal text-white">Descripción de cada materia</p>
                 </a>
-                <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm 
-                    hover:border-4 hover:border-blue-900 hover:bg-gray-100 dark:bg-blue-800 dark:border-blue-900 
-                    dark:hover:bg-blue-700 transition-all duration-3000 ease-in-out">
-                    <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Programación</h6>
-                    <p class="font-normal text-white dark:text-white">Descripción de cada materia</p>
-                </a>
-                <!-- añadir mas materias -->
-                
-                
+                <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md
+                            dark:bg-blue-800 dark:border-blue-900 
+                            dark:hover:bg-blue-600 transition-all duration-[1000ms] ease-in-out
+                            hover:shadow-xl hover:border-blue-900">
+                    <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Matemáticas</h6>
+                    <p class="font-normal text-white">Descripción de cada materia</p>
+                </a>        
             </div>
         </div>
     </section>
