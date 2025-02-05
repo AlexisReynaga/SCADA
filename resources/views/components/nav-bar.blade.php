5a82be079c52adea444a6c5d88cfa734ffe1bfd2
ferchas293855 <ferchas2015@gmail.com>
2025-02-04 18:25:48 -0600

    agregue una mejor interfaz visual al navbar y al header para que se vea mejor

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,18 +1,18 @@
 <!-- Botón para abrir/cerrar -->
-<button id="toggleSidebar" class="fixed top-5 left-5 bg-blue-900 text-white p-2 rounded z-50 transition-all">☰</button>
+<button id="toggleSidebar" class="fixed top-5 left-3 bg-blue-800 text-white p-3 rounded z-50 transition-all">☰</button>
 
 <!-- Overlay -->
 <div id="overlay" class="hidden fixed inset-0 bg-black bg-opacity-50 z-40"></div>
 
 <div class="flex">
     <!-- Sidebar -->
-    <div id="sidebar" class="fixed top-0 left-0 w-64 bg-blue-900 text-white h-screen p-5 transition-transform transform -translate-x-full z-50">
+    <div id="sidebar" class="fixed top-0 left-0 w-64 bg-blue-800 text-white h-screen p-5 transition-transform transform -translate-x-full z-50">
         <h2 class="text-xl font-semibold">Menú</h2>
         <ul class="mt-4 space-y-2">
-            <li><a href="{{ route('home') }}" class="block p-2 hover:bg-blue-800 rounded">Inicio</a></li>
-            <li><a href="{{ route('perfil') }}" class="block p-2 hover:bg-blue-800 rounded">Perfil</a></li>
-            <li><a href="{{ route('bitacora')}}" class="block p-2 hover:bg-blue-800 rounded">Bitacora</a></li>
-            <li><a href="{{ route('materias')}}" class="block p-2 hover:bg-blue-800 rounded">Materias</a></li>
+            <li><a href="{{ route('home') }}" class="block p-2 hover:bg-blue-500 rounded">Inicio</a></li>
+            <li><a href="{{ route('perfil') }}" class="block p-2 hover:bg-blue-500 rounded">Perfil</a></li>
+            <li><a href="{{ route('bitacora')}}" class="block p-2 hover:bg-blue-500 rounded">Bitacora</a></li>
+            <li><a href="{{ route('materias')}}" class="block p-2 hover:bg-blue-500 rounded">Materias</a></li>
         </ul>
     </div>
 </div>
