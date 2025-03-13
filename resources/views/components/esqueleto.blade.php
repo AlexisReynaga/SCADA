1bf2f5df61b5798d9c87b5b7325d1df7373260ab
Monse27142406 <104400515+Monse27142406@users.noreply.github.com>
2025-03-13 10:29:04 -0600

    Alta de usuarios

--- resources/views/components/esqueleto.blade.php
+++ resources/views/components/esqueleto.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SCADA</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </head>
 <x-header></x-header>
 <body>
